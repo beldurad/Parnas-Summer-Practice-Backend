03f12f4601fc63ff884ac9c427a5a56720265cda
Ilya <ignatium1356@gmail.com>
2026-07-11 16:48:43 +0300

    initial commit added openapi doc

---
+++ src/main/java/org/example/parnasservice/ParnasServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.parnasservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParnasServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParnasServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parnasservice/ParnasServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parnasservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParnasServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
